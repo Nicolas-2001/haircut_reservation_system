c0285214f5f65e72dd91e55704c8e4d5c7a8a6d8
Nicolas Pena <161085691+Nicolas-2001@users.noreply.github.com>
2026-04-09 22:55:51 -0500

    feat: add custom error classes for MySQL error handling
    Add NullFieldError, DataTooLongError, InvalidValueError, DataOutOfRangeError,
    ForeignKeyNotFoundError, ReferencedRowError and DatabaseError, each with
    their corresponding HTTP status code.

--- utils/error.js
+++ utils/error.js
@@ -7,6 +7,69 @@ class DuplicateEntryError extends Error {
 	}
 }
 
+class NullFieldError extends Error {
+	constructor(field = "unknown") {
+		super(`The field '${field}' cannot be null.`);
+		this.name = "NullFieldError";
+		this.statusCode = 400;
+	}
+}
+
+class DataTooLongError extends Error {
+	constructor(field = "unknown") {
+		super(`The value for '${field}' exceeds the maximum allowed length.`);
+		this.name = "DataTooLongError";
+		this.statusCode = 400;
+	}
+}
+
+class InvalidValueError extends Error {
+	constructor(field = "unknown") {
+		super(`Invalid value provided for '${field}'.`);
+		this.name = "InvalidValueError";
+		this.statusCode = 400;
+	}
+}
+
+class DataOutOfRangeError extends Error {
+	constructor(field = "unknown") {
+		super(`The value for '${field}' is out of the allowed range.`);
+		this.name = "DataOutOfRangeError";
+		this.statusCode = 400;
+	}
+}
+
+class ForeignKeyNotFoundError extends Error {
+	constructor() {
+		super("The referenced record does not exist.");
+		this.name = "ForeignKeyNotFoundError";
+		this.statusCode = 400;
+	}
+}
+
+class ReferencedRowError extends Error {
+	constructor() {
+		super("Cannot delete or update: the record is referenced by other records.");
+		this.name = "ReferencedRowError";
+		this.statusCode = 409;
+	}
+}
+
+class DatabaseError extends Error {
+	constructor() {
+		super("Database service is currently unavailable.");
+		this.name = "DatabaseError";
+		this.statusCode = 503;
+	}
+}
+
 module.exports = {
 	DuplicateEntryError,
+	NullFieldError,
+	DataTooLongError,
+	InvalidValueError,
+	DataOutOfRangeError,
+	ForeignKeyNotFoundError,
+	ReferencedRowError,
+	DatabaseError,
 };
